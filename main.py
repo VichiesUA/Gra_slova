e77d2104a26226b71ca5616406ed1418959bb36b
VichiesUA <stasyatsyn@gmail.com>
2022-08-30 21:34:53 +0300

    123

---
+++ main.py
@@ -0,0 +1,40 @@
+class Animals:
+    def __init__(self, name, age, colour):
+        self.name = name
+        self.age = age
+        self.colour = colour
+
+    def get_name(self):
+        print(f'Ім\'я собаки ==> {self.name}')
+
+    def get_age(self):
+        print(f'Вік собаки ==> {self.age}')
+
+    def get_colour(self):
+        print(f'Колір собаки ==> {self.colour}')
+
+
+class Birds(Animals):
+    def __init__(self, name, age, colour, speed):
+        super().__init__(name, age, colour)
+        self.speed = speed
+
+    def get_name(self):
+        print(f'Ім\'я пташки ==> {self.name}')
+
+    def get_age(self):
+        print(f'Вік пташки ==> {self.age}')
+
+    def get_colour(self):
+        print(f'Колір пташки ==> {self.colour}')
+
+    def get_speed(self):
+        print(f'Швидкість пташки ==> {self.speed}')
+
+
+cat = Birds(input('Введіть ім\'я пташки ==> '), int(input('Введіть вік пташки ==> ')),
+            input('Введіть колір пташки ==> '), int(input('Введіть швидкість пташки ==> ')))
+cat.get_name()
+cat.get_age()
+cat.get_colour()
+cat.get_speed()
