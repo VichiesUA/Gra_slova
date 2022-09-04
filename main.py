cfe55476c62dc4f4341d7b8787c9c861875557d3
VichiesUA <stasyatsyn@gmail.com>
2022-09-04 14:46:10 +0300

    Merge remote-tracking branch 'main1.py/homework_stas_yatsyn_main' into homework_stas_yatsyn_main

--- main.py
+++ main.py
@@ -1,33 +1,62 @@
 name_list = []
 word_list = []
+res = {}
 
-name = None
+print('''----------------------------
+Вас вітає гра в "слова"
+Для початку введіть імена гравців. 
+Напишіть "start" для початку гри.
+----------------------------''')
 
 
 def player_name():
     while True:
-        name = input('Введіть імя')
-        if name == 'Start':
+        name = input('Введіть і\'мя: \n')
+        if name == 'start':
             break
-        else:
-            name_list.append(name)
+
+        name_list.append(name.upper())
+        res.setdefault(name.upper(), 0)
 
 
-def func():
+def word():
     while True:
         for i in name_list:
-            slovo = input(f'{i}, введи будь ласка слово')
-            while slovo in word_list:
-                if slovo in word_list:
-                    print('це слово вже використовувалось, введіть інше')
-                    slovo = input(f'{i}, введи будь ласка слово')
+            if len(word_list) == 0:
+                slovo = input(f'{i}, введіть слово: \n').lower()
+                word_list.append(slovo)
+                res[i] += 1
+            else:
+                last_word = str(word_list[-1])
+                slovo = input(f'{i}, введіть слово на букву "{last_word[-1]}": \n').lower()
+                if slovo == 'stop':
+                    for key, value in res.items():
+                        print(f' {key} ввів стільки слів ==> {value}')
+                    print('-------------------------------')
+                    print(f'Всього слів ==> {len(word_list)}')
+                    exit()
                 else:
-                    word_list.append(slovo)
-                    break
-
-            word_list.append(slovo)
+                    while True:
+                        if slovo == 'stop':
+                            for key, value in res.items():
+                                print(f' {key} ввів стільки слів ==> {value}')
+                            print('-------------------------------')
+                            print(f'Всього слів ==> {len(word_list)}')
+                            exit()
+                        elif not slovo.startswith(last_word[-1]):
+                            print('Це слово починається з іншої букви!')
+                            slovo = input(f'{i}, введіть слово на букву "{last_word[-1]}": \n').lower()
+                        elif slovo in word_list:
+                            print('Це слово вже використовувалось, введіть інше: \n')
+                            slovo = input(f'{i}, введіть слово на букву "{last_word[-1]}": \n').lower()
+                        else:
+                            word_list.append(slovo)
+                            res[i] += 1
+                            break
 
 
 player_name()
-func()
-print(name_list, word_list)
+print('''--------------------------------
+Гра розпочалась! Щоб закінчити гру напишіть "stop".
+--------------------------------''')
+word()
